66131b904ab0d6b00c86b6cb7c020165965f8c7a
mhmdhirza02 <mhmd.hirza027@students.amikom.ac.id>
2026-07-27 10:05:38 +0700

    sql

--- config/database.php
+++ config/database.php
@@ -96,7 +96,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => env('DB_SSLMODE', 'require'),
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
